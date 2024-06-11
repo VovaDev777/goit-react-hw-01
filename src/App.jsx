399a3a04603c8e956699c100d42b86bc93a7e269
VovaDev777 <polvovater@gmail.com>
2024-06-11 19:02:56 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 import Profile from "./components/Profile/Profile"
-
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308
-  }
-}
+import userData from './components/Profile/userData.json'
 
 
 const App = () => {
